857037d2e4a1a8e77aa186b544b07d6aca94974d
Richard <hello@noxan.net>
2017-07-05 23:04:05 +0200

    split github related code into a subfolder

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,12 @@
-import axios from 'axios';
+import { getIssues } from './github';
 
-const baseUrl = 'https://api.github.com';
 const project = {
   scope: 'noxan',
   name: 'gi-cli',
 };
 
-const getIssues = async (authToken: any) => {
-  const response = await axios.post(`${baseUrl}/graphql`, {
-    query: `{
-      repository(owner: "${project.scope}", name: "${project.name}") {
-        issues(last: 100) {
-          edges {
-            node {
-              number,
-              title
-            }
-          }
-        }
-      }
-    },`
-  }, {
-    headers: {
-      Authorization: `bearer ${authToken}`,
-    },
-  });
-  if (response.data) {
-    return response.data.data.repository.issues.edges.map((node: any) => node.node);
-  }
-  return response;
-}
-
 const main = async () => {
-  const issues = await getIssues(process.env.AUTH_TOKEN);
+  const issues = await getIssues(project, process.env.AUTH_TOKEN);
   console.log(issues.map((issue: any) => `${issue.number} - ${issue.title}`));
 };
 
