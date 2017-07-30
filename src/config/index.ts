33abd6db475812cac170bd580d6dd88b93f576f5
Richard <hello@noxan.net>
2017-07-30 15:57:29 +0200

    parse content of config file with yaml

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
 import { homedir } from 'os';
+import { safeLoad } from 'js-yaml';
 
 export const configPath = join(homedir(), '.gi.yaml');
 
@@ -8,5 +9,5 @@ export const configFileExists = () => existsSync(configPath);
 
 export const loadConfigFile = () => {
   const content = readFileSync(configPath, 'utf-8');
-  return content;
+  return safeLoad(content);
 };
