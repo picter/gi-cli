6102e49b0fddd280b28a4271128dc1017bd3f56c
Richard <hello@noxan.net>
2017-07-30 15:35:27 +0200

    use some more random user directory mock to make sure it should not exist

--- src/config/index.spec.ts
+++ src/config/index.spec.ts
@@ -2,12 +2,12 @@ jest.mock('os');
 
 const os = require('os');
 os.homedir = jest.fn();
-os.homedir.mockReturnValue('/Users/demo');
+os.homedir.mockReturnValue('/Users/gi-demo-cf07295117d2');
 
 import { configPath, configFileExists } from '.';
 
 test('Config path is within user home directory.', () => {
-  expect(configPath).toEqual('/Users/demo/.gi.yaml');
+  expect(configPath).toEqual('/Users/gi-demo-cf07295117d2/.gi.yaml');
 });
 
 test('Check if config file exists.', () => {
