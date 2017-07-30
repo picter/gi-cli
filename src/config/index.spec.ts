051643b1858f4a289fb0786b5e3d91d34d98247c
Richard <hello@noxan.net>
2017-07-30 15:54:19 +0200

    mock fs

--- src/config/index.spec.ts
+++ src/config/index.spec.ts
@@ -1,8 +1,15 @@
 jest.mock('os');
+jest.mock('fs');
 
 const os = require('os');
 os.homedir = jest.fn();
 os.homedir.mockReturnValue('/Users/gi-demo-cf07295117d2');
+
+const fs = require('fs');
+fs.existsSync = jest.fn();
+fs.existsSync.mockReturnValue(false);
+fs.readFileSync = jest.fn();
+fs.readFileSync.mockReturnValue('bla');
 
 import { configPath, configFileExists } from '.';
 
