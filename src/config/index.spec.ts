758f2b632acfc9b36887b1cbb35f40ce1a8e6247
Richard <hello@noxan.net>
2017-07-30 15:32:34 +0200

    add func to check if config file exists

--- src/config/index.spec.ts
+++ src/config/index.spec.ts
@@ -4,8 +4,12 @@ const os = require('os');
 os.homedir = jest.fn();
 os.homedir.mockReturnValue('/Users/demo');
 
-import { configPath } from '.';
+import { configPath, configFileExists } from '.';
 
 test('Config path is within user home directory.', () => {
   expect(configPath).toEqual('/Users/demo/.gi.yaml');
+});
+
+test('Check if config file exists.', () => {
+  expect(configFileExists()).toBe(false);
 });
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,7 @@
+import { existsSync } from 'fs';
 import { join } from 'path';
 import { homedir } from 'os';
 
 export const configPath = join(homedir(), '.gi.yaml');
+
+export const configFileExists = () => existsSync(configPath);
