2943fac021f19f572ac3e123494360c06f1e35a6
Richard <hello@noxan.net>
2017-11-22 19:12:47 +0100

    create empty pull request command

---
+++ src/commands/pull-request.ts
@@ -0,0 +1,10 @@
+import { Arguments } from 'yargs';
+
+const pullRequestCommand = async (
+  command: string,
+  project: any,
+  args: Arguments,
+  authToken: string,
+) => {};
+
+export default pullRequestCommand;
