ed52e994b3c0abb0984da82e8fc4a32a7ec616ec
Richard <hello@noxan.net>
2017-11-22 18:49:24 +0100

    extract project name and scope from remote url (added two test cases)

--- src/remotes/index.ts
+++ src/remotes/index.ts
@@ -1,12 +1,16 @@
 import { getRemoteUrl } from './package';
 
-export default async () => {
-  const remote = await getRemoteUrl();
-
-  console.log(remote);
-
+export const extractFromRemoteUrl = (remoteUrl: string) => {
+  const strippedRemoteUrl = remoteUrl.split(':').pop() || remoteUrl;
+  const parts = strippedRemoteUrl.replace(/\.git$/, '').split('/');
   return {
-    scope: 'noxan',
-    name: 'gi-cli',
+    name: parts.pop(),
+    scope: parts.pop(),
   };
+};
+
+export default async () => {
+  const remoteUrl = await getRemoteUrl();
+
+  return extractFromRemoteUrl(remoteUrl);
 };
