5b5dda2524d62bcc727aec2c5c8f9a59c3c7f487
Richard <hello@noxan.net>
2017-11-22 16:51:08 +0100

    fix test case of 0 to return unknown command

--- src/commands/index.spec.ts
+++ src/commands/index.spec.ts
@@ -1,11 +1,11 @@
-import { selectCommand, list, checkout } from '.';
+import { selectCommand, list, checkout, unknown } from '.';
 
 test('Empty command parameter results in list command.', () => {
   expect(selectCommand('')).toEqual(list);
 });
 
-test('0 as command parameter results in list command.', () => {
-  expect(selectCommand('0')).toEqual(list);
+test('0 as command parameter results in unknown command.', () => {
+  expect(selectCommand('0')).toEqual(unknown);
 });
 
 test('1 as command parameter results in checkout command.', () => {
