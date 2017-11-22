4015791de38ebae5c336a4c7758dbcd6732e20a9
Richard <hello@noxan.net>
2017-11-22 20:09:32 +0100

    fix default command and make cli work again

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const config = loadConfigFile();
 const argv = yargs
   .usage('gi [command]')
   .command('<issue number>', 'Checkout branch for issue number.')
-  .command('$0 list', 'Lists all open issues of this project.', y =>
+  .command('list', 'Lists all open issues of this project.', y =>
     y.option('all', { describe: 'Show all issues', default: false }),
   )
   .command(['pr', 'merge'], 'Create pull/merge request for current branch.')
