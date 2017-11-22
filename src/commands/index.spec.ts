0ee947045e05c3a71b968c6d8939ad8ec0e20848
Richard <hello@noxan.net>
2017-11-22 19:27:43 +0100

    add test case for merge command

--- src/commands/index.spec.ts
+++ src/commands/index.spec.ts
@@ -1,4 +1,4 @@
-import { selectCommand, list, checkout, unknown } from '.';
+import { selectCommand, list, checkout, merge, unknown } from '.';
 
 test('Empty command parameter results in list command.', () => {
   expect(selectCommand('')).toEqual(list);
@@ -18,4 +18,8 @@ test('1 as command parameter results in checkout command.', () => {
 
 test('Single space as command parameter results in unknown command.', () => {
   expect(selectCommand(' ')).toEqual(unknown);
+});
+
+test('Merge command parameter results in merge command.', () => {
+  expect(selectCommand('merge')).toEqual(merge);
 });
