488f32e31ced1e1253e972ddb96158c1fe3b5ed5
Richard <hello@noxan.net>
2017-11-22 19:18:40 +0100

    construct github pull request url based on project name and scope

--- src/commands/pull-request.ts
+++ src/commands/pull-request.ts
@@ -5,6 +5,12 @@ const pullRequestCommand = async (
   project: any,
   args: Arguments,
   authToken: string,
-) => {};
+) => {
+  const branchName = 'branchName';
+  const projectUrl = `https://github.com/${project.scope}/${project.name}`;
+  const url = `${projectUrl}/compare/master...${branchName}`;
+
+  console.log(url);
+};
 
 export default pullRequestCommand;
