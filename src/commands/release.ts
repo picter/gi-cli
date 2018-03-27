170e2c0834a82d2df1d630bc8279bde3f3cde237
Richard <hello@noxan.net>
2018-03-27 16:46:08 +0200

    warn or throw error on wrong branches

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -1,4 +1,5 @@
 import { Arguments } from 'yargs';
+import * as git from 'simple-git/promise';
 
 const releaseCommand = async (
   command: string,
@@ -6,6 +7,16 @@ const releaseCommand = async (
   args: Arguments,
   authToken: string,
 ) => {
+  const repository = git(process.cwd());
+  const status = await repository.status();
+
+  const branchName = status.current;
+  if (branchName === 'release') {
+    throw new Error('Cannot create release for release branch.');
+  } else if (branchName !== 'master') {
+    console.error('WARNING: You should release from master branch.');
+  }
+
   console.log(`Release "${command}, ${args}".`);
 };
 
