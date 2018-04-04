bf65688c4b30a8f0e99540908feed4b15980c68e
Richard <hello@noxan.net>
2018-04-04 15:14:48 +0200

    add test case for issue title with `

--- src/git/index.spec.ts
+++ src/git/index.spec.ts
@@ -3,3 +3,13 @@ import { slugifyTitle } from '.';
 test('Convert regular issue title to branch name (slugify).', () => {
   expect(slugifyTitle('Sanitize branch name')).toEqual('sanitize-branch-name');
 });
+
+test('Convert issue title with ` to branch name (slugify).', () => {
+  expect(
+    slugifyTitle(
+      'Implement flag props like `nopaddings` and `nomargins` on text components',
+    ),
+  ).toEqual(
+    'implement-flag-props-like-nopaddings-and-nomargins-on-text-components',
+  );
+});
