6ec7b0f75de61061d2df9450939444c00eb99bea
Richard <hello@noxan.net>
2018-04-06 17:12:21 +0200

    extract release branch name (productionBranchname)

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -2,6 +2,8 @@ import { Arguments } from 'yargs';
 import * as opn from 'opn';
 import * as git from 'simple-git/promise';
 
+const productionBranchname = '';
+
 const releaseCommand = async (
   command: string,
   project: any,
@@ -12,14 +14,16 @@ const releaseCommand = async (
   const status = await repository.status();
 
   const branchName = status.current;
-  if (branchName === 'release') {
-    throw new Error('Cannot create release for release branch.');
+  if (branchName === productionBranchname) {
+    throw new Error(
+      `Cannot create release for ${productionBranchname} branch.`,
+    );
   } else if (branchName !== 'master') {
     console.error('WARNING: You should release from master branch.');
   }
 
   const projectUrl = `https://github.com/${project.scope}/${project.name}`;
-  const url = `${projectUrl}/compare/release...${branchName}`;
+  const url = `${projectUrl}/compare/${productionBranchname}...${branchName}`; // Use new branch name
 
   console.log(url);
   await opn(url, { wait: false });
