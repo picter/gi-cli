3f8a977c4b0b0979ac9fd1f8b85efce52835726b
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-04-06 15:28:21 -0300

    Finish writing tests for release

--- src/commands/release.spec.ts
+++ src/commands/release.spec.ts
@@ -2,29 +2,62 @@ import { Arguments } from 'yargs';
 
 import release from './release';
 
-jest.mock('simple-git/promise', () => ((pwd: string) => {
+const mockCheckoutLocalBranch = jest.fn();
+jest.mock('simple-git/promise', () => (pwd: string) => {
   return {
-    status: jest.fn()
-    .mockResolvedValueOnce({ current: 'master' })
-    .mockResolvedValueOnce({ current: 'production' })
-    .mockResolvedValueOnce({ current: 'random' })
+    status: jest
+      .fn()
+      .mockResolvedValueOnce({ current: 'master' })
+      .mockResolvedValueOnce({ current: 'production' })
+      .mockResolvedValueOnce({ current: 'random' }),
+    checkoutLocalBranch: mockCheckoutLocalBranch,
   };
-}));
+});
 
-const mockOpn = jest.fn().mockResolvedValueOnce('try');
-jest.mock('opn', () => ((url: string) => mockOpn));
+
+const mockWritePkg = jest.fn(() => Promise.resolve());
+jest.mock('write-pkg', () => (args: any) => mockWritePkg(args));
+
+const mockOpn = jest.fn();
+jest.mock('opn', () => (url: string) => mockOpn(url));
+
+const buildPullRequestUrl = (
+  project: any,
+  productionBranch: string,
+  releaseBranch: string,
+) =>
+  `https://github.com/${project.scope}/${
+    project.name
+  }/compare/${productionBranch}...${releaseBranch}`;
 
 describe('release command', () => {
-  const project = 'arbitrary-project';
+  const project = {
+    name: 'project',
+    scope: 'picter',
+  };
   const argv = { _: [], $0: '' };
   const token = '';
 
   describe('when receives a specific version number', () => {
     const version = '1.1.1';
-    release(version, project, argv, token);
+    const releaseBranch = `release-${version}`;
 
-    it('creates a new branch with "release-x.y.z"', () => {});
-    it('writes the version to package.json', () => {});
-    it('calls "opn" with the pull request url', () => {});
+    beforeAll(() => {
+      release(version, project, argv, token);
+    });
+
+    it('creates a new branch with "release-x.y.z"', () => {
+      expect(mockCheckoutLocalBranch).toHaveBeenCalledWith(releaseBranch);
+    });
+
+    it('writes the version to package.json', () => {
+      const packageJson = { version };
+      expect(mockWritePkg).toHaveBeenCalledWith(packageJson);
+    });
+
+    it('calls "opn" with the pull request url', () => {
+      const pullRequestUrl = buildPullRequestUrl(project, 'production', releaseBranch);
+      expect(mockOpn).toHaveBeenCalledWith(pullRequestUrl);
+    });
   });
 });
